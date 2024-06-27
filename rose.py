963002d9c33f7c4c1161317f7de11bba2fc3dbed
lxc <1951381703@qq.com>
2024-06-27 20:51:01 +0800

    The Beauty of Graphics Implement using the turtle package Use the Turtle package to achieve some stunning effects
    Open source
    protocol is GPLv2.0

---
+++ rose.py
@@ -0,0 +1,22 @@
+import random
+import turtle
+
+turtle.pensize(1)
+turtle.speed(-1)
+turtle.colormode(255)
+turtle.bgcolor((0, 0, 0))
+
+step = 0
+i = 0
+for i in range(300):
+    i = i
+    turtle.pencolor((random.randint(255, 255), random.randint(0, 255), random.randint(0, 255)))
+    if i % 5 == 0:
+        step += 1
+    turtle.forward(i + step)
+    turtle.left(70)
+# turtle.forward(i + step + 1)
+
+turtle.hideturtle()
+turtle.down()
+turtle.mainloop()
